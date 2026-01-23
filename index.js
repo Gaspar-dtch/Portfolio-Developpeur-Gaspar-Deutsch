d920cd8884ce402c20929883124c06bf7dd5c4cd
Gaspar <gaspar.deutsch@gmail.com>
2026-01-23 01:27:49 +0100

    :wrench: fix - hamburger menu

--- index.js
+++ index.js
@@ -1,21 +1,22 @@
-const lenis = new Lenis({
-    duration: 1.2,
-    easing: (t) => 1 - Math.pow(1 - t, 3),
-    smoothWheel: true,
-    smoothTouch: false,
-});
-
-function raf(time) {
-    lenis.raf(time);
-    requestAnimationFrame(raf);
-}
-
-requestAnimationFrame(raf);
-
-
 const toggle = document.querySelector('.nav-toggle');
 const links = document.querySelector('.nav-links');
 
 toggle.addEventListener('click', () => {
     links.classList.toggle('active');
 });
+
+
+
+// const lenis = new Lenis({
+//     duration: 1.2,
+//     easing: (t) => 1 - Math.pow(1 - t, 3),
+//     smoothWheel: true,
+//     smoothTouch: false,
+// });
+
+// function raf(time) {
+//     lenis.raf(time);
+//     requestAnimationFrame(raf);
+// }
+
+// requestAnimationFrame(raf);
