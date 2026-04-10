6881cd10679f52d0478f811651eee27212391c95
Gaspar <gaspar.deutsch@gmail.com>
2026-04-10 18:53:55 +0200

    :wrench: worshop on all style project

--- index.js
+++ index.js
@@ -7,16 +7,17 @@ toggle.addEventListener("click", () => {
 
 
 
-// const lenis = new Lenis({
-//     duration: 1.2,
-//     easing: (t) => 1 - Math.pow(1 - t, 3),
-//     smoothWheel: true,
-//     smoothTouch: false,
-// });
 
-// function raf(time) {
-//     lenis.raf(time);
-//     requestAnimationFrame(raf);
-// }
+const lenis = new Lenis({
+    duration: 1.2,
+    easing: (t) => 1 - Math.pow(1 - t, 3),
+    smoothWheel: true,
+    smoothTouch: false,
+});
 
-// requestAnimationFrame(raf);
+function raf(time) {
+    lenis.raf(time);
+    requestAnimationFrame(raf);
+}
+
+requestAnimationFrame(raf);
